54972960d272756dca23d6322f1805351b3a2370
Mengwa Ugo <mengwa44@yahoo.com>
2025-02-05 15:54:11 +0100

    add redux store

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {store} from '../redux/store'
+import { Provider } from "react-redux";
+
+
+
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +31,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+      <Provider store={store}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+
+      </Provider>
     </html>
   );
 }
