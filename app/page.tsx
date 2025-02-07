3b4be452b9f01aaf8eb6ea062602b4a27c91ffeb
Mengwa Ugo <mengwa44@yahoo.com>
2025-02-07 11:12:45 +0100

    docs: testing to upload

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ export default function Home() {
     <>
       <h1>
         Welcome to <a href="https://nextjs.org">Next.js</a>
-        test
-
-        testing git 
+      
         
       </h1>
     </>
