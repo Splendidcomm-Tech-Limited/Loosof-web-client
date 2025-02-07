21f9cc28d30371535e995158487ddd61735cb0cf
Mengwa Ugo <mengwa44@yahoo.com>
2025-02-07 10:16:33 +0100

    adding features

--- components/sales/nav-bar.tsx
+++ components/sales/nav-bar.tsx
@@ -1,5 +1,5 @@
-import { MountainIcon } from 'lucide-react'
-import Link from 'next/link'
+
+
 import React from 'react'
 
 export default function SalesNavbar() {
@@ -7,45 +7,7 @@ export default function SalesNavbar() {
     <nav>
       {/* navbar */}
 
-      <div className="flex items-center justify-end px-4 py-2 bg-white dark:bg-gray-800">
-        <div className="hidden md:flex gap-4">
-          <Link
-            href="#"
-            className="text-lg font-medium hover:underline underline-offset-4"
-            prefetch={false}
-          >
-            Home
-          </Link>
-          <Link
-            href="#"
-            className="text-lg font-medium hover:underline underline-offset-4"
-            prefetch={false}
-          >
-            About
-          </Link>
-          <Link
-            href="#"
-            className="text-lg font-medium hover:underline underline-offset-4"
-            prefetch={false}
-          >
-            Services
-          </Link>
-          <Link
-            href="#"
-            className="text-lg font-medium hover:underline underline-offset-4"
-            prefetch={false}
-          >
-            Portfolio
-          </Link>
-          <Link
-            href="#"
-            className="text-lg font-medium hover:underline underline-offset-4"
-            prefetch={false}
-          >
-            Contact
-          </Link>
-        </div>
-      </div>
+      
     </nav>
   )
 }
