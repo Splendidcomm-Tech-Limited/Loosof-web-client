3204077009f94cadd8a7f7e8f8c87ea3fa406326
Mengwa Ugo <mengwa44@yahoo.com>
2025-02-13 10:40:03 +0100

    style(component): tble

---
+++ components/accounting/department-column.tsx
@@ -0,0 +1,146 @@
+'use client'
+
+import { ColumnDef } from '@tanstack/react-table'
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Checkbox } from '../ui/checkbox'
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Department = {
+  id: string
+  department_name: string
+  description: string
+  created_by: string
+  created_at: string
+  status: 'active' | 'inactive'
+}
+
+export const columns: ColumnDef<Department>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value : any) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'department_name',
+    enableHiding: false,
+    header: ({ column }) => {
+      return (
+        <div className='flex  items-center' >
+          Depart. Name
+          <ArrowUpDown onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="ml-2 h-4 w-4 cursor-pointer" />
+        </div>
+      )
+    },
+  },
+  {
+    accessorKey: 'description',
+    header: ({ column }) => {
+      return (
+        <div className='flex  items-center' >
+          Description
+          <ArrowUpDown onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="ml-2 h-4 w-4 cursor-pointer" />
+        </div>
+      )
+    },
+  },
+  {
+    accessorKey: 'created_at',
+    header: ({ column }) => {
+      return (
+        <div className='flex  items-center' >
+          Created At
+          <ArrowUpDown onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="ml-2 h-4 w-4 cursor-pointer" />
+        </div>
+      )
+    },
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('created_at'))
+      const formatted = date.toLocaleDateString()
+      return <div>{formatted}</div>
+    },
+  },
+  {
+    accessorKey: 'created_by',
+    header: ({ column }) => {
+      return (
+        <div className='flex  items-center' >
+          Created By
+          <ArrowUpDown onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="ml-2 h-4 w-4 cursor-pointer" />
+        </div>
+      )
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: ({ column }) => {
+      return (
+        <div className='flex  items-center' >
+          Status
+          <ArrowUpDown onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')} className="ml-2 h-4 w-4 cursor-pointer" />
+        </div>
+      )
+    },
+    cell: ({ row }) => {
+      const data = row.getValue('status')
+      return <div>{data === 'female' ? 'active' : 'inactive'}</div>
+    },
+  },
+  {
+    id: 'actions',
+    enableSorting: false,
+    enableHiding: false,
+    header: 'Action',
+    cell: ({ row }) => {
+      const department = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem onClick={() => navigator.clipboard.writeText(department.id)}>
+              Copy department ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Delete department</DropdownMenuItem>
+            <DropdownMenuItem>Edit department</DropdownMenuItem>
+            <DropdownMenuItem>Duplicate department</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
--- components/accounting/department-data-table.tsx
+++ components/accounting/department-data-table.tsx
@@ -1,52 +1,128 @@
-"use client"
-
-
-import React from 'react';
+'use client'
+import * as React from 'react'
 import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-  } from "@tanstack/react-table"
-   
-  import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  SortingState,
+  getSortedRowModel,
+  getPaginationRowModel,
+  getFilteredRowModel,
+  VisibilityState,
+  ColumnFiltersState,
+  useReactTable,
+} from '@tanstack/react-table'
 
-  interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
-  }
-  export function DataTable<TData, TValue>({
-    columns,
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { Download, Settings2 } from 'lucide-react'
+import { DataTablePagination } from './shared/pagination'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+  const table = useReactTable({
     data,
-  }: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-    })
-   
-    return (
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
+
+  return (
+    <>
+      <div className="flex justify-between items-center ml-1  py-4">
+        <Input
+          placeholder="Filter Departments..."
+          value={(table.getColumn('department_name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn('department_name')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <div className="flex gap-2 mr-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                <Settings2 /> Add Columns
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value: any) => column.toggleVisibility(!!value)}
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  )
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="ml-auto">
+                <Download /> Download
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              <DropdownMenuItem>Pdf</DropdownMenuItem>
+              <DropdownMenuItem>Csv</DropdownMenuItem>
+              <DropdownMenuItem>Xsls</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
       <div className="rounded-md border">
         <Table>
-          <TableHeader className='bg-primary opacity-40'>
+          <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow  key={headerGroup.id}>
+              <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead className='text-black font-semibold text-lg' key={header.id}>
+                    <TableHead key={header.id}>
                       {header.isPlaceholder
                         ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                        : flexRender(header.column.columnDef.header, header.getContext())}
                     </TableHead>
                   )
                 })}
@@ -56,10 +132,7 @@ import {
           <TableBody>
             {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
@@ -77,5 +150,10 @@ import {
           </TableBody>
         </Table>
       </div>
-    )
-  }
+
+      <div className="pb-10">
+        <DataTablePagination table={table} />
+      </div>
+    </>
+  )
+}
